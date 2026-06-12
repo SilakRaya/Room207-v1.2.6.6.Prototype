9ae7aa2e232c60a52974984b6329a635f09354c9
x40arcade <sobrepenakenneth97@gmail.com>
2026-06-12 12:06:10 +0800

    1.6.3.6
    Fixed button hover animation

--- room-207-prototype/MainMenu/button_anim.gd
+++ room-207-prototype/MainMenu/button_anim.gd
@@ -1,72 +1,66 @@
 extends Node
 
+var HOVER_SCALE := Vector2(1.2, 1.2)
+var NORMAL_SCALE := Vector2.ONE
 var tween:Tween
+var buttons:Array[Button]
+var button_tweens := {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().process_frame
-	setup_button_pivots()
-
-func setup_button_pivots() -> void:
-	var buttons = [%NewGame,%Options,%Credits,%Exit]
-	for button in buttons:
-		#button.anchor_left = 0.5
-		#button.anchor_right = 0.5
-		#button.anchor_top = 0.5
-		#button.anchor_bottom = 0.5
-		button.pivot_offset = button.size / 2
 	
+	buttons = [%NewGame,%Options,%Credits,%Exit]
+	
+	for button in buttons:
+		button.pivot_offset = button.size / 2
+
+func animate_button(button: Control, target_scale: Vector2, duration: float) -> void:
+	if button_tweens.has(button):
+		var old_tween = button_tweens[button]
+		if old_tween:
+			old_tween.kill()
+	var tween := create_tween()
+	button_tweens[button] = tween
+	tween.set_trans(Tween.TRANS_QUINT)
+	tween.set_ease(Tween.EASE_OUT)
+	tween.tween_property(button, "scale", target_scale, duration)
+
+func set_hovered_button(hovered: Control) -> void:
+	for button in buttons:
+		if button == hovered:
+			animate_button(button, HOVER_SCALE, 0.25)
+		else:
+			animate_button(button, NORMAL_SCALE, 0.20)
+
 
 func _on_new_game_mouse_entered() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%NewGame, "scale", Vector2(1.2, 1.2), 0.5)
-	 
+	set_hovered_button(%NewGame)
+
+
 func _on_new_game_mouse_exited() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%NewGame, "scale", Vector2(1.0, 1.0), 0.3)
+	animate_button(%NewGame, NORMAL_SCALE, 0.20)
 
 
 func _on_options_mouse_entered() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%Options, "scale", Vector2(1.2, 1.2), 0.5)
+	set_hovered_button(%Options)
 
 
 func _on_options_mouse_exited() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%Options, "scale", Vector2(1.0, 1.0), 0.3)
+	animate_button(%Options, NORMAL_SCALE, 0.20)
 
 
 func _on_credits_mouse_entered() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%Credits, "scale", Vector2(1.2, 1.2), 0.5)
+	set_hovered_button(%Credits)
 
 
 func _on_credits_mouse_exited() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%Credits, "scale", Vector2(1.0, 1.0), 0.3)
+	animate_button(%Credits, NORMAL_SCALE, 0.20)
 
 
 func _on_exit_mouse_entered() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%Exit, "scale", Vector2(1.2, 1.2), 0.5)
+	set_hovered_button(%Exit)
 
 
 func _on_exit_mouse_exited() -> void:
-	if tween: tween.kill()
-	tween = create_tween()
-	tween.set_trans(Tween.TRANS_QUINT)
-	tween.tween_property(%Exit, "scale", Vector2(1.0, 1.0), 0.3)
+	animate_button(%Exit, NORMAL_SCALE, 0.20)
